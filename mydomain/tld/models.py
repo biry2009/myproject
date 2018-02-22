e7a1c1bcfd0e6b9de67b1dea8a7e8ebeb52cfbea
biry2009@gmail.com <biry2009@gmail.com>
2018-02-22 18:43:07 +0800

    change css style

--- mydomain/tld/models.py
+++ mydomain/tld/models.py
@@ -27,6 +27,8 @@ class Registrar(models.Model):
     keyword = models.CharField(max_length=20)
     name = models.CharField(max_length=30)
     url = models.CharField(max_length=50)
+    color = models.CharField(max_length=10, default="#fff")
+    review = models.TextField(blank=True)
     rating = models.FloatField(blank=True)
 
     def __str__(self):
